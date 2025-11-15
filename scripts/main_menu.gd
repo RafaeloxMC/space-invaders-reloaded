f095e6853391e6d5d3c470c726b4c0002503ad35
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-11-15 13:01:28 +0100

    Add player

---
+++ scripts/main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@export var game_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if !game_scene:
+		print("WARNING: No game scene set!")
+	pass # Replace with function body.
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_packed(game_scene)
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
