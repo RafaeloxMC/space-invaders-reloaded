24ec63948319e0725972abf24d65109246f063c0
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-11-16 05:37:53 +0100

    Add reload on death

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var enemy: PackedScene
 
+var reloading: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,8 +11,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if !self.find_child("Player") && !reloading:
+		reloading = true
+		Engine.time_scale = 0.5
+		await get_tree().create_timer(2).timeout
+		Engine.time_scale = 1
+		get_tree().reload_current_scene()
+	
 	if randi_range(0, 1000) == 999:
 		var projectile = enemy.instantiate() as CharacterBody2D
 		projectile.global_position = self.global_position
 		projectile.position.y += 20
 		self.add_sibling(projectile)
+	
